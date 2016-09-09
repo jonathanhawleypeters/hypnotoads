dc1640f073c3edf8ab1c9569d8f11d5e3ca53566
Jonathan Hawley-Peters <jhawleypeters@gmail.com>
2016-09-09 12:11:31 -0700

    add authentication logic

--- workout-app/app.js
+++ workout-app/app.js
@@ -42,4 +42,72 @@ angular.module('workout-app', [
 
       $httpProvider.interceptors.push('AttachTokens');
 
+  })
+  .factory('Auth', function ($http, $location, $window) {
+    var isAuth = function () {
+      return !!$window.localStorage.getItem('hypnotoad');
+    };
+    return {
+      isAuth: isAuth
+    };
+  })
+  .factory('AttachTokens', function ($window) {
+    var attach = {
+      request: function (object) {
+        var jwt = $window.localStorage.getItem('hypnotoad');
+        if (jwt) {
+          object.headers['x-access-token'] = jwt;
+        }
+        object.headers['Allow-Control-Allow-Origin'] = '*';
+        return object;
+      }
+    };
+    return attach;
+  })
+  .controller('AuthController', function ($scope, $window, $location, Auth, $http) {
+    $scope.checkUser = function (username, password) {
+      return $http({
+        method: 'POST',
+        url: '/signin',
+        data: { username: username, password: password }
+      })
+      .then(function (res) {
+        console.log('Debug output', res.data.token);
+        $window.localStorage.setItem('hypnotoad', res.data.token);
+        $window.localStorage.setItem('username', username);
+        $location.path('/profile');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    };
+    $scope.addUser = function (username, password) {
+      return $http({
+        method: 'POST',
+        url: '/signup',
+        data: { username: username, password: password }
+      })
+      .then(function (res) {
+        $window.localStorage.setItem('hypnotoad', res.data.token);
+        $window.localStorage.setItem('username', username);
+        $location.path('/profile');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+    };
+    $scope.signout = function () {
+      console.log('logging out');
+      $window.localStorage.removeItem('hypnotoad');
+      $window.localStorage.removeItem('username');
+      $location.path('/signin');
+    };
+  })
+  .run(function ($rootScope, $location, Auth) {
+    $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+      console.log('routeChangeStart in app.js');
+      if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+        $location.path('/signin');
+      }
+    });
   });
